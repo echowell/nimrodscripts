8c0ca7568580ce0b399b60e880d0096a14de7ecb
Eric Howell <ehowell@txcorp.com>
2019-03-28 16:53:12 -0600

    Update transport scripts

--- transport_calculator/neoclassical_coeff.py
+++ transport_calculator/neoclassical_coeff.py
@@ -3,4 +3,46 @@
 import numpy as np
 import plasma_constants as pc
 
-print (pc.mu0)
+
+### set background plasma parameters
+ne = 6.82e19 #m^-3
+te = 1504. #eV
+ti = 1754. #eV
+zeff = 3.25
+ni = 3.49e19 #m^-3 rough guess
+
+epsilon = 0.25 # magnetic inverse aspect ratio
+R0 = 1.696
+q0 = 2.0
+
+#calcularte classical values for electrons and ions with impurities
+#here I assume the the Coulomb Log is the same for both electron and 
+#ions
+logLam = pc.coulombLog(te,ne)
+nu_e = pc.nu_e(ne,te,zeff,logLam)
+vte = pc.vte(te)
+lambda_e = vte/nu_e
+zStar=pc.zStar(ne,ni,zeff)
+nu_i = pc.nu_i(ni,ti,pc.md,zStar,logLam)
+vti = pc.vts(ti,pc.md) 
+lambda_i = vti/nu_i
+
+fc = pc.circulatingFraction(epsilon)#circuling fraction
+nueBanana = pc.nuBanana(nu_e,epsilon)
+nuiBanana = pc.nuBanana(nu_i,epsilon)
+
+nueStar = pc.nuStar(R0,q0,epsilon,lambda_e)
+nuiStar = pc.nuStar(R0,q0,epsilon,lambda_i)
+mu_e = pc.neoMue(nu_e,nueStar,zeff,epsilon)
+mu_i = pc.neoMue(nu_i,nuiStar,zStar,epsilon)
+print(logLam)
+print(nu_e)
+print(nu_i)
+print (lambda_e)
+print (lambda_i)
+print(nu_i/nu_e)
+print(fc)
+print(nueStar)
+print(nuiStar)
+print(mu_e)
+print(mu_i)
--- transport_calculator/plasma_constants.py
+++ transport_calculator/plasma_constants.py
@@ -8,3 +8,52 @@ mu0 = 4 * np.pi * 1e-7
 gamma = 5./3.
 kboltz = 1.60217733e-19
 clight = 2.99792458e8
+eps0 = 8.854187817e-12
+ 
+def coulombLog(te, ne):
+#  logLam = 18.4 - 1.15 * np.log10(ne)+2.3*np.log10(te)
+# Jim Callen uses 17
+  logLam = 24 - np.log(np.sqrt(ne*1.e-6)/te)
+  return logLam
+
+def nu_e(ne,te,zeff,logLam):
+  nu = np.sqrt(2*np.pi)* ne * zeff * qe**4 * logLam
+  nu *= 1.0/(12.0*(np.pi * eps0)**2 * np.sqrt(me) * np.power(kboltz*te,1.5))
+  return nu
+
+def nu_i(ni,ti,mi,zstar,logLam):
+  nu = np.sqrt(np.pi)* ni* (1.0+np.sqrt(2.0)*zstar)  * qe**4 * logLam
+  nu *= 1.0/(12.0*(np.pi * eps0)**2 * np.sqrt(mi) * np.power(kboltz*ti,1.5))
+  return nu
+
+def vte(te):
+  return np.sqrt(2*kboltz*te/me)
+
+def vts(ts,ms):
+  return np.sqrt(2*kboltz*ts/ms)
+
+def zStar(ne,ni,zeff):
+  return (ne/ni)*zeff-1.0
+
+def circulatingFraction(eps):
+  return 1-1.46*np.sqrt(eps) + 0.46 * np.power(eps,1.5)
+
+def nuBanana(nu,eps):
+  return np.sqrt(eps)*nu
+
+def nuStar(R0,q0,epsilon,lam):
+  return R0*q0/(np.power(epsilon,1.5)*lam)
+
+def neoMue(nue,nueStar,zeff,eps):
+  numer = 1.46 * np.sqrt(eps) * (1.+0.533/zeff)*nue
+  denomFac = (2.4*zeff**2+5.32*zeff+2.225)/(4.25*zeff**2+3.02*zeff)
+  denom = (1.+np.sqrt(nueStar)+1.65*(1.+0.533/zeff)*nueStar)
+  denom *= (1+1.18*denomFac*np.power(eps,1.5)*nueStar)
+  return numer/denom
+
+def neoMui(nu,nuStar,zStar,eps):
+  numer = 1.46 * np.sqrt(eps) * (zStar+0.533)/(zStar+0.707)*nu
+  denomFac = (2.4*zStar**2+5.32*zStar+2.225)/((zStar+0.707)*(4.25*zStar+3.02))
+  denom = (1.+np.sqrt(nuStar)+1.65*(zStar+0.533)/(zStar+0.707)*nuStar)
+  denom *= (1+1.18*denomFac*np.power(eps,1.5)*nuStar)
+  return numer/denom
