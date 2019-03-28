c5a208a593e84a6438320779dd2f10b7aad555c2
Eric Howell <ehowell@txcorp.com>
2019-03-28 14:18:03 -0600

    Add file to hold physical constants used in plasma physics.

--- transport_calculator/neoclassical_coeff.py
+++ transport_calculator/neoclassical_coeff.py
@@ -1,3 +1,6 @@
 #!/usr/local/bin/python3
 
 import numpy as np
+import plasma_constants as pc
+
+print (pc.mu0)
---
+++ transport_calculator/plasma_constants.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+import numpy as np
+me = 9.1093898e-31
+md = 3.3435860e-27
+qe = 1.60217733e-19
+mu0 = 4 * np.pi * 1e-7
+gamma = 5./3.
+kboltz = 1.60217733e-19
+clight = 2.99792458e8
