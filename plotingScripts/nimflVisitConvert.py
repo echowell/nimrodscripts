ba425de113841f6a9fca7326643ad2e88b3e614e
Eric Howell <ehowell@txcorp.com>
2019-04-04 12:04:05 -0600

    Add script to convert nimfl.dat file to a 3D file format that visit can read.

---
+++ plotingScripts/nimflVisitConvert.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+''' This script reads a reads a nimfl.dat file and converts it to a plot 3D'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+import os
+
+homeDir = os.environ['HOME']
+relDir = "/SCRATCH/166439/footpoint_03300_q104/lphi5/S7Pr1e2/"
+fileName = "nimfl0000000.dat"
+plot3Dfile = "nimfl000000.3D"
+
+fullFile = homeDir+relDir+fileName
+fullOutFile = homeDir+relDir+plot3Dfile
+
+pssData = np.loadtxt(fullFile)
+print(pssData.shape)
+
+f = open(fullOutFile, "wt")
+f.write("x y z value\n")
+for ii in range(pssData.shape[0]):
+  f.write("%g %g %g %g\n" % (pssData[ii,0],pssData[ii,1],0.0,pssData[ii,2]))
+f.close()
